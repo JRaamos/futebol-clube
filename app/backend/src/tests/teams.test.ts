0695491215d221eceb220574d459877395592d02
JRaamos <jhonyramos46@gmail.com>
2023-11-08 02:42:59 -0300

    feat: 04 - (TDD) Desenvolve testes que cubram no mínimo 10 por cento dos arquivos em /app/backend/src, com um mínimo de 19 linhas cobertas

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -8,6 +8,7 @@ import Example from '../database/models/ExampleModel';
 import TeamsModel from '../database/models/TeamsModel';
 import { Response } from 'superagent';
 import { findAllResult } from './mocks/teams.mock';
+import e from 'express';
 chai.use(chaiHttp);
 
 const { expect } = chai;
@@ -29,4 +30,15 @@ describe('Teams Test rota /teams',function () {
         expect(status).to.be.eq(200);
         expect(body).to.deep.equal(findAllResult);
 
-})});
+})
+it('GET /teams/:id, deve devolver um obj com id e nome do time', async function () {
+  sinon
+    .stub(TeamsModel, "findOne")
+    .resolves(
+      findAllResult[0] as TeamsModel);
+  const { status, body } = await chai.request(app).get('/teams/1');
+
+      expect(status).to.be.eq(200);
+      expect(body).to.deep.equal(findAllResult[0]);
+})
+});
