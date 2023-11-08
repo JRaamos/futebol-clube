e03c2e5289eea6a0d35ff3f80b9da48085609159
JRaamos <jhonyramos46@gmail.com>
2023-11-08 18:39:52 -0300

    feat: 13 - Desenvolve em /app/backend/src/database nas pastas correspondentes, uma migration e um model para a tabela de partidas

---
+++ app/backend/src/database/migrations/03-create-matches.ts
@@ -0,0 +1,43 @@
+import { Model, QueryInterface, DataTypes } from "sequelize";
+import { IMatches } from "../../Interfaces/matches/IMatches";
+
+export default {
+  up(QueryInterface: QueryInterface) {
+    return QueryInterface.createTable<Model<IMatches>>('matches', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      homeTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'home_team_id',
+      },
+      homeTeamGoals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'home_team_goals',
+      },
+      awayTeamId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'away_team_id',
+      },
+      awayTeamGoals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'away_team_goals',
+      },
+      inProgress: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        field: 'in_progress',
+      },
+    })
+  },
+  down(QueryInterface: QueryInterface) {
+    return QueryInterface.dropTable('matches');
+  },
+}
--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -7,13 +7,15 @@ import { Response } from 'superagent';
 chai.use(chaiHttp);
 // import validationJWT from '../middlewares/validationJWT';
 import User from '../database/models/UserModel';
-import { INVALID_DATA, user, userIvalid, passwordIvalid, validUser, notData, NOT_DATA } from './mocks/users.mock';
+import {
+  user, userIvalid, passwordIvalid, validUser, notData,
+  INVALID_DATA, NOT_DATA
+} from './mocks/users.mock';
 const { expect } = chai;
-
 
 describe('Users Test', () => {
   it("O avaliador verificará se é possível fazer o login com dados corretos", async function () {
-    sinon.stub(User, 'findOne').resolves(validUser as User);    
+    sinon.stub(User, 'findOne').resolves(validUser as User);
     const { status, body } = await chai.request(app).post('/login').send(user);
 
     expect(status).to.be.eq(200);
@@ -21,33 +23,32 @@ describe('Users Test', () => {
     sinon.restore();
   });
   it('O avaliador verificará nao pe possivel fazer o login com dados de email incorretos', async function () {
-    sinon.stub(User, 'findOne').resolves(userIvalid as User);    const { status, body } = await chai.request(app).post('/login').send(user);
-  
+    sinon.stub(User, 'findOne').resolves(userIvalid as User);
+    const { status, body } = await chai.request(app).post('/login').send(user);
+
     expect(status).to.be.eq(401);
     expect(body.message).to.equal(INVALID_DATA.data.message);
     sinon.restore();
   });
 
   it('O avaliador verificará se é lançado um erro caso a senha tenha o tamanho menor que 6', async function () {
-    sinon.stub(User, 'findOne').resolves(passwordIvalid as User); 
-    
+    sinon.stub(User, 'findOne').resolves(null);
+
 
     const { status, body } = await chai.request(app).post('/login').send(passwordIvalid);
 
-  
+
     expect(status).to.be.eq(401);
     expect(body.message).to.equal(INVALID_DATA.data.message);
     sinon.restore();
   });
   it('O avaliador verificará se é lançado um erro caso o email nao exista', async function () {
-    sinon.stub(User, 'findOne').resolves(notData as User); 
-    
-
+    sinon.stub(User, 'findOne').resolves(null);
     const { status, body } = await chai.request(app).post('/login').send(notData);
 
-  
+
     expect(status).to.be.eq(400);
     expect(body.message).to.equal(NOT_DATA.data.message);
     sinon.restore();
   });
-  });
+});
