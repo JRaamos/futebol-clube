80784b1f43ed7284bd5ced09bc2239baa3e5d11f
JRaamos <jhonyramos46@gmail.com>
2023-11-09 02:07:45 -0300

    feat: 16 - Desenvolve o endpoint /matches de forma que seja possível filtrar somente as partidas em andamento, e também filtrar somente as partidas finalizadas, na tela de partidas do front-end

--- app/backend/src/controllers/Metches.controller.ts
+++ app/backend/src/controllers/Metches.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { IMatches } from '../Interfaces/matches/IMatches';
 import mapStatusHTTP from '../utils/mapStatusHTTP';
 import MatchesService from '../services/Matches.service';
 
@@ -8,7 +9,16 @@ export default class MatchesController {
   ) { }
 
   public async findAll(req: Request, res: Response) {
+    const { inProgress } = req.query;
     const { status, data } = await this.matchesService.findAll();
+    console.log(inProgress);
+
+    if (inProgress) {
+      const inProgressBoolean = inProgress === 'true';
+      const matchesInProgress = JSON.parse(JSON.stringify(data))
+        .filter((match: IMatches) => match.inProgress === inProgressBoolean);
+      return res.status(mapStatusHTTP(status)).json(matchesInProgress);
+    }
     return res.status(mapStatusHTTP(status)).json(data);
   }
 }
